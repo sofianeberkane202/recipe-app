e94826c0a8112b4376abe6d98ee1aa89d2fd330a
sofiane berkane <sofianeberkane202@gmail.com>
2024-10-05 22:24:14 +0100

    update controllerSavedRecipes.js the function controllerLoadSavedRecipes that will return array of saved recipes data

--- assets/js/Controlers/controllerSavedRecipes.js
+++ assets/js/Controlers/controllerSavedRecipes.js
@@ -1,8 +1,11 @@
 import { ControllerBase } from "./controllerBase";
 import savedRecipes from "../Views/savedViews/savedRecipes";
 import * as model from "../models/model";
+import { wait } from "../global";
 class ControllerSavedRecipes extends ControllerBase {
   #parentElement = document.querySelector("[data-saved-recipe-container]");
+  #savedRecipesData = [];
+
   constructor() {
     super();
     this.initBase();
@@ -13,10 +16,26 @@ class ControllerSavedRecipes extends ControllerBase {
     try {
       await model.fetchRecipeSavedData();
 
-      model.state.recipeSavedData.forEach(async (recipeId) => {
-        await model.fetchData(recipeId);
-        savedRecipes.render(model.state.data, model.state.recipeSavedData);
-      });
+      model.state?.recipeSavedData?.size &&
+        savedRecipes.renderSkeleton(
+          undefined,
+          model.state.recipeSavedData.size
+        );
+
+      this.#savedRecipesData = [...model.state.recipeSavedData].map(
+        async (recipeId) => {
+          const w = wait(0.1);
+          await w.promise;
+          clearTimeout(w.timeoutId);
+
+          await model.fetchData(recipeId);
+          return model.state.data;
+        }
+      );
+
+      this.#savedRecipesData = await Promise.all(this.#savedRecipesData);
+
+      savedRecipes.render(this.#savedRecipesData, model.state.recipeSavedData);
     } catch (error) {}
   }
 
@@ -27,7 +46,9 @@ class ControllerSavedRecipes extends ControllerBase {
   }
 
   init() {
-    savedRecipes.addHandlerLoadSavedRecipes(this.controllerLoadSavedRecipes);
+    savedRecipes.addHandlerLoadSavedRecipes(
+      this.controllerLoadSavedRecipes.bind(this)
+    );
     savedRecipes.addHandlerSaveRecipe(this.controllerSaveRecipe);
   }
 }
